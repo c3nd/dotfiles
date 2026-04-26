7fa50dc8b6d9c32d21cb79c1babd5d002b134a53
c3nd <ascendancyluvsu@gmail.com>
2026-04-25 18:01:23 -0700

    packages.nix

--- packages.nix
+++ packages.nix
@@ -5,11 +5,14 @@ with pkgs; [
     kitty
     app2unit
     foot
+    equibop
     fastfetch
     lm_sensors
     strawberry
     kdePackages.kate
+    kdePackages.qt6ct
     kdePackages.dolphin
+    kdePackages.dolphin-plugins
     kdePackages.qtsvg
     kdePackages.kio-fuse
     kdePackages.kio-extras
