158ac0f26f7ef96feac2c0d59cf0dbb4096d7a78
c3nd <ascendancyluvsu@gmail.com>
2026-08-06 10:33:07 -0700

    cassiopeia: unpin nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Cassiopeia Dendritic-style Dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
     nh.url = "github:nix-community/nh";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     openwhispr.url = "github:OpenWhispr/OpenWhispr";
