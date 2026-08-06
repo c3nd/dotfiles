4badf3eb74b28513276d163ca6c65a9e9512fd4f
c3nd <109569073+c3nd@users.noreply.github.com>
2026-08-05 23:54:57 -0700

    Revert "Cassiopeia dendritic"

--- packages.nix
+++ packages.nix
@@ -104,16 +104,6 @@ with pkgs;
   zlib
 
   ############################################################################
-  # CAD / aerospace design stack  (FreeCAD, meshing, FEA, 2D drafting, etc.)
-  ############################################################################
-  freecad
-  gmsh
-  calculix-ccx
-  librecad
-  openscad
-  python313Packages.pythonocc-core
-
-  ############################################################################
   # Misc / runtime libraries & tools
   ############################################################################
   upower
