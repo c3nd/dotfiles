1e697ab2c3c9a48dbe5227ec680ff14c72a0ad62
c3nd <ascendancyluvsu@gmail.com>
2026-08-06 10:47:48 -0700

    cassiopeia: remove stale paths, SecondSpot mount, faster-whisper refs, fix qt6ct path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,23 +1,5 @@
 #!/usr/bin/env fish
-# Cassiopeia fish config: aliases + faster-whisper X11 dictation helpers
-
-function faster-whisper-toggle
-  set STATE_DIR (test -n "$XDG_STATE_HOME"; and echo $XDG_STATE_HOME; or echo "$HOME/.local/state")/faster-whisper
-  set SCRIPT "$HOME/dotfiles/config/faster-whisper-toggle.sh"
-  if test -f "$STATE_DIR/.recording"
-    $SCRIPT stop
-  else
-    $SCRIPT start
-  end
-end
-
-function faster-whisper-postprocess
-  "$HOME/dotfiles/config/faster-whisper-toggle.sh" postprocess
-end
-
-function faster-whisper-cancel
-  "$HOME/dotfiles/config/faster-whisper-toggle.sh" cancel
-end
+# Cassiopeia fish config
 
 alias btw "echo i use nixos, btw"
 alias nsw "nh os switch"
@@ -27,4 +9,3 @@ alias nrb "nh os rollback"
 alias nif "nh os info"
 alias ndry "nh os switch --dry"
 alias ncl "nh clean all -k 3"
-alias plspush "cd ~/dotfiles; and git add -A; and git commit -m 'autocommit'; and git push; and cd ~"
