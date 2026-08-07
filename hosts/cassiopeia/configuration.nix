10287d38ef244a0e6cc8cb99f0f42b4db69a8487
c3nd <ascendancyluvsu@gmail.com>
2026-08-06 17:08:57 -0700

    cassiopeia: remove duplicate boot.kernelPackages from configuration.nix

--- hosts/cassiopeia/configuration.nix
+++ hosts/cassiopeia/configuration.nix
@@ -13,7 +13,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernelModules = [ "kvm-intel" ];
 
   networking.hostName = "Cassiopeia";
